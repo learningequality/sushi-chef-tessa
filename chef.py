b17825b207168d17929b273c871377b4cfbcbb3f
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2017-05-31 15:07:48 -0700

    more example

--- chef.py
+++ chef.py
@@ -1,17 +1,23 @@
 import requests
-import tempfile
 from bs4 import BeautifulSoup
 
 from ricecooker.classes.nodes import ChannelNode, HTML5AppNode, DocumentNode
-from ricecooker.classes.files import HTMLZipFile, DocumentFile, ThumbnailFile
+from ricecooker.classes.files import HTMLZipFile, DocumentFile
 from ricecooker.utils.caching import CacheForeverHeuristic, FileCache, CacheControlAdapter, InvalidatingCacheControlAdapter
 
 from le_utils.constants import licenses
 
 from ricecooker.utils.html import download_file
-from ricecooker.utils.zip import create_predictable_zip
 
-def construct_channel(*args, **kwargs):
+def create_channel_for_language(language, language_url_map):
+    if language not in language_url_map:
+        return
+
+    try:
+        top_level_page = requests.get(language_url_map[language])
+    except:
+        # TODO(arvind): log error
+        return
 
     channel = ChannelNode(
         source_domain="tessafrica.net",
@@ -22,7 +28,6 @@ def construct_channel(*args, **kwargs):
 
     soup = BeautifulSoup(open('example_page.html').read())
     downloads = soup.find(id="downloads")
-    # import ipdb; ipdb.set_trace()
     links = [a for a in downloads.find_all("a") if a['href'].endswith('html.zip') and a.find(class_="oucontent-title")]
     if not links:
         return channel
@@ -33,8 +38,16 @@ def construct_channel(*args, **kwargs):
 
     doc = HTML5AppNode(source_id=source_id, title=title, files=[HTMLZipFile(path=url)], license=licenses.CC_BY_SA)
     channel.add_child(doc)
-
     return channel
+
+
+def construct_channel(*args, **kwargs):
+
+    language_url_map = {
+        "en": "http://www.open.edu/openlearncreate/course/view.php?id=2042"
+    }
+
+    return create_channel_for_language("en", languages)
 
 if __name__ == '__main__':
     construct_channel()
